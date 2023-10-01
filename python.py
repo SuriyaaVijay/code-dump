d461d792f5b7fc675361c61d1162e8f8bcdc2452
Dipalikamboj <93436913+Dipalikamboj@users.noreply.github.com>
2023-10-01 17:21:03 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,11 @@
+# Python program to print negative Numbers in a List
+
+# list of numbers
+list1 = [11, -21, 0, 45, 66, -93]
+
+# iterating each number in list
+for num in list1:
+
+	# checking condition
+	if num < 0:
+	print(num, end=" ")
