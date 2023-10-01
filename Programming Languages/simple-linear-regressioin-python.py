0b37f48c069f587b921b1424e6ad9939c9d77520
Sneha2s <146506201+Sneha2s@users.noreply.github.com>
2023-10-01 17:34:43 +0530

    Update simple-linear-regressioin-python.py

--- Programming Languages/simple-linear-regressioin-python.py
+++ Programming Languages/simple-linear-regressioin-python.py
@@ -1,29 +1,54 @@
 import numpy as np
-import pandas as pd
-
-class MeraLR:
-    
-    def __init__(self):
-        self.m = None
-        self.b = None
-        
-    def fit(self,X_train,y_train):
-        
-        num = 0
-        den = 0
-        
-        for i in range(X_train.shape[0]):
-            
-            num = num + ((X_train[i] - X_train.mean())*(y_train[i] - y_train.mean()))
-            den = den + ((X_train[i] - X_train.mean())*(X_train[i] - X_train.mean()))
-        
-        self.m = num/den
-        self.b = y_train.mean() - (self.m * X_train.mean())
-        print(self.m)
-        print(self.b)       
-    
-    def predict(self,X_test):
-        
-        print(X_test)
-        
-        return self.m * X_test + self.b
+import matplotlib.pyplot as plt
+ 
+def estimate_coef(x, y):
+    # number of observations/points
+    n = np.size(x)
+ 
+    # mean of x and y vector
+    m_x = np.mean(x)
+    m_y = np.mean(y)
+ 
+    # calculating cross-deviation and deviation about x
+    SS_xy = np.sum(y*x) - n*m_y*m_x
+    SS_xx = np.sum(x*x) - n*m_x*m_x
+ 
+    # calculating regression coefficients
+    b_1 = SS_xy / SS_xx
+    b_0 = m_y - b_1*m_x
+ 
+    return (b_0, b_1)
+ 
+def plot_regression_line(x, y, b):
+    # plotting the actual points as scatter plot
+    plt.scatter(x, y, color = "m",
+               marker = "o", s = 30)
+ 
+    # predicted response vector
+    y_pred = b[0] + b[1]*x
+ 
+    # plotting the regression line
+    plt.plot(x, y_pred, color = "g")
+ 
+    # putting labels
+    plt.xlabel('x')
+    plt.ylabel('y')
+ 
+    # function to show plot
+    plt.show()
+ 
+def main():
+    # observations / data
+    x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    y = np.array([1, 3, 2, 5, 7, 8, 8, 9, 10, 12])
+ 
+    # estimating coefficients
+    b = estimate_coef(x, y)
+    print("Estimated coefficients:\nb_0 = {}  \
+          \nb_1 = {}".format(b[0], b[1]))
+ 
+    # plotting regression line
+    plot_regression_line(x, y, b)
+ 
+if _name_ == "_main_":
+    main()
