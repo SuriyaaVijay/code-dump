f7a079bbb70fc451f28437a1780d7f0d5d2e776d
Abhay Pandey <116704975+Aloneking789@users.noreply.github.com>
2023-10-02 20:01:02 +0530

    Insertion_sort.py
    Solution to the problem #129 Insertion_sort
    Here's an implementation of the insertion sort algorithm in Python

---
+++ Insertion_sort.py
@@ -0,0 +1,16 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+
+        # Move elements of arr[0..i-1] that are greater than key
+        # to one position ahead of their current position
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+# Example usage:
+my_list = [12, 11, 13, 5, 6]
+insertion_sort(my_list)
+print("Sorted array:", my_list)
