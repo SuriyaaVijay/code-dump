a457ddd2782623a2d4d8ed90434fa79c9f7ce1f7
bstrachn <93625934+bstrachn@users.noreply.github.com>
2023-10-02 10:24:56 -0400

    Create Linkedlist.py
    demonstrates a Linked List using python

---
+++ Algorithms/Linkedlist.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+
+    def prepend(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, end=' ')
+            current = current.next
+        print()
+
+# Example usage
+if __name__ == "__main__":
+    linked_list = LinkedList()
+    linked_list.append(1)
+    linked_list.append(2)
+    linked_list.append(3)
+    linked_list.prepend(0)
+    linked_list.print_list()  # Output: 0 1 2 3
