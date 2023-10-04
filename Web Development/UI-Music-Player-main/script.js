089abf10706250bd1f58a46a08d1e1cff76bb175
Ratnadeep Bhowmik <ratnadeep.bhowmik2021@vitstudent.ac.in>
2023-10-04 20:14:18 +0530

    added myself

---
+++ Web Development/UI-Music-Player-main/script.js
@@ -0,0 +1,6 @@
+const mod = document.querySelector('#mode');
+
+mod.addEventListener('click', () => {
+    document.querySelector('.phone').classList.toggle('dark');
+    document.querySelector('.bi-brightness-high-fill').classList.toggle('bi-moon');
+}); 
