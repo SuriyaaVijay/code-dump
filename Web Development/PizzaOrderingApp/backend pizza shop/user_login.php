f4b4ef7ec8f3d1f21beaf656e4db833eab1cfdbd
Tushar Bansal <89680344+tusharban42@users.noreply.github.com>
2023-10-07 02:19:03 -0700

    Add files via upload

---
+++ Web Development/PizzaOrderingApp/backend pizza shop/user_login.php
@@ -0,0 +1,27 @@
+<?php
+
+include 'config.php';
+
+session_start();
+
+if(isset($_POST['login'])){
+
+   $email = $_POST['email'];
+   $email = filter_var($email, FILTER_SANITIZE_STRING);
+   $pass = sha1($_POST['pass']);
+   $pass = filter_var($pass, FILTER_SANITIZE_STRING);
+
+   $select_user = $conn->prepare("SELECT * FROM `user` WHERE email = ? AND password = ?");
+   $select_user->execute([$email, $pass]);
+   $row = $select_user->fetch(PDO::FETCH_ASSOC);
+
+   if($select_user->rowCount() > 0){
+      $_SESSION['user_id'] = $row['id'];
+      header('location:index.php');
+   }else{
+      header('location:index.php');
+   }
+
+}
+
+?>
---
+++ Web Development/PizzaOrderingApp/backend pizza shop/users_accounts.php
@@ -0,0 +1,75 @@
+<?php
+
+include 'config.php';
+
+session_start();
+
+$admin_id = $_SESSION['admin_id'];
+
+if(!isset($admin_id)){
+   header('location:admin_login.php');
+};
+
+if(isset($_GET['delete'])){
+   $delete_id = $_GET['delete'];
+   $delete_order = $conn->prepare("DELETE FROM `user` WHERE id = ?");
+   $delete_order->execute([$delete_id]);
+   header('location:users_accounts.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>users accounts</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom admin style link  -->
+   <link rel="stylesheet" href="css/admin_style.css">
+
+</head>
+<body>
+
+<?php include 'admin_header.php' ?>
+
+<section class="accounts">
+
+   <h1 class="heading">user accounts</h1>
+
+   <div class="box-container">
+
+   <?php
+      $select_accounts = $conn->prepare("SELECT * FROM `user`");
+      $select_accounts->execute();
+      if($select_accounts->rowCount() > 0){
+         while($fetch_accounts = $select_accounts->fetch(PDO::FETCH_ASSOC)){   
+   ?>
+   <div class="box">
+      <p> user id : <span><?= $fetch_accounts['id']; ?></span> </p>
+      <p> username : <span><?= $fetch_accounts['name']; ?></span> </p>
+      <p> email : <span><?= $fetch_accounts['email']; ?></span> </p>
+      <a href="users_accounts.php?delete=<?= $fetch_accounts['id']; ?>" onclick="return confirm('delete this account?')" class="delete-btn">delete</a>
+   </div>
+   <?php
+         }
+      }else{
+         echo '<p class="empty">no accounts available!</p>';
+      }
+   ?>
+
+   </div>
+
+</section>
+
+
+
+<script src="js/admin_script.js"></script>
+
+</body>
+</html>
