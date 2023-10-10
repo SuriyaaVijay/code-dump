89ad9501aa95eeb135b15dd8edc9abafa5ba8fe9
Vasav Chaudhary <115100723+vasav11@users.noreply.github.com>
2023-10-10 13:15:27 +0530

    Create BST Operations.java

---
+++ Java/BST Operations.java
@@ -0,0 +1,82 @@
+public class BinarySearchTree {
+
+    private Node root;
+
+    class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+    
+	public Node getRoot() {
+        return root;
+    }
+    
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+        if (root == null) {
+            root = newNode;
+            return true;
+        }
+        Node temp = root;
+        while (true) {
+            if (newNode.value == temp.value) return false;
+            if (newNode.value < temp.value) {
+                if (temp.left == null) {
+                    temp.left = newNode;
+                    return true;
+                }
+                temp = temp.left;
+            } else {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
+    }
+public boolean contains(int value){
+ Node temp=root;
+ while(temp!=null){
+     if(value<temp.value){
+         temp=temp.left;
+     }else if (value>temp.value){
+         temp=temp.right;
+     }else{
+         return true;
+     }
+ }
+ return false;
+}
+}
+public class Main {
+
+    public static void main(String[] args) {
+
+        BinarySearchTree myBST = new BinarySearchTree();
+
+        myBST.insert(2);
+        myBST.insert(1);
+        myBST.insert(3);
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(27);
+        myBST.insert(52);
+        myBST.insert(82);
+
+
+        System.out.println("BST Contains 27:");
+        System.out.println(myBST.contains(27));
+
+        System.out.println("\nBST Contains 17:");
+        System.out.println(myBST.contains(17));
+  }
+
+}
