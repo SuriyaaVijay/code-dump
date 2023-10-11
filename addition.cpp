65a1a44b76521e7e881dbcbec8ac9f1228ba3667
JAYGBHADRA2005 <146119340+JAYGBHADRA2005@users.noreply.github.com>
2023-10-11 21:17:58 +0530

    Addition.cpp
    This C++ program explains the code for the addition of two numbers!

---
+++ addition.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+  int a,b;
+  
+  cout<<"Enter the value of a"<<endl;
+  cin>>a;
+  cout<<"Enter the value of b"<<endl;
+  cin>>b;
+  int c=a+b;
+  cout<<"The addition of two numbers is: ";
+  cout c;
+  
+
+
+}
