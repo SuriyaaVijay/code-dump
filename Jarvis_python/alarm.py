1fa988643885b2479014a440df4bdab88d6674de
Sridhar1030 <77533416+Sridhar1030@users.noreply.github.com>
2023-10-13 22:49:04 +0530

    Add files via upload

---
+++ Jarvis_python/alarm.py
@@ -0,0 +1,38 @@
+import pyttsx3
+import datetime
+import os 
+
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[0].id)
+engine.setProperty("rate",200)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+extractedtime = open("Alarmtext.txt","rt")
+time = extractedtime.read()
+Time = str(time)
+extractedtime.close()
+
+deletetime = open("Alarmtext.txt","r+")
+deletetime.truncate(0)
+deletetime.close()
+
+def ring(time):
+    timeset = str(time)
+    timenow = timeset.replace("jarvis","")
+    timenow = timenow.replace("set an alarm","")
+    timenow = timenow.replace(" and ",":")
+    Alarmtime = str(timenow)
+    print(Alarmtime)
+    while True:
+        currenttime = datetime.datetime.now().strftime("%H:%M:%S")
+        if currenttime == Alarmtime:
+            speak("Alarm ringing,sir")
+            os.startfile("music.mp3")
+        elif currenttime + "00:00:30" == Alarmtime:
+            exit()
+
+ring(time)
