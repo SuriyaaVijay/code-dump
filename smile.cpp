e33e3f7620c1819bf5f89cdbdee994f3784b72f0
Abhinav Tiwari <97847842+AbhinavTiwari2705@users.noreply.github.com>
2023-10-16 01:09:27 +0530

    Create smile.cpp

---
+++ smile.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "  *****  " << std::endl;
+    std::cout << " *     * " << std::endl;
+    std::cout << "*  O O  *" << std::endl;
+    std::cout << "*   ^   *" << std::endl;
+    std::cout << " *     * " << std::endl;
+    std::cout << "  *****  " << std::endl;
+
+    return 0;
+}
