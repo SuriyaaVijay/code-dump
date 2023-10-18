ac9aef0f1cbc3bdec49ad4c373c163e173faeb27
Lívia Buriti <92129840+buritizinhw@users.noreply.github.com>
2023-10-17 23:26:27 -0300

    Create QuickSort.java

---
+++ Algorithms/QuickSort.java
@@ -0,0 +1,43 @@
+public class QuickSort {
+
+    public static void main(String[] args) {
+        int[] arr = {9, 7, 5, 11, 12, 2, 14, 3, 10, 6};
+        
+        quickSort(arr, 0, arr.length - 1);
+
+        System.out.println("Array ordenado:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                // Troca arr[i] e arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // Troca arr[i+1] e arr[high] (o pivô)
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+}
