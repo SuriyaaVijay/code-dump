0bd3b868f3cb1e5492953c8aebda1bdcf6635675
Adithya Gayashan <adithyagayashan02@gmail.com>
2023-10-22 22:25:50 +0530

    quick sort algorithm in python

---
+++ Python/sorting/quickSort.py
@@ -0,0 +1,10 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+
+    return quick_sort(left) + middle + quick_sort(right)
