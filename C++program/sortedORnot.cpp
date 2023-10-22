94fbe7ec35a5e230b74a118e803b12bdadc060a2
Anushka Priyadarshi <139917503+StarlyHere@users.noreply.github.com>
2023-10-23 00:35:21 +0530

    Create sortedORnot.cpp
    Checks if array is sorted or not

---
+++ C++program/sortedORnot.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+    int a[6]={1,2,5,3,3,4};
+    for(int i=1;i<6;i++){
+        if(a[i]>=a[i-1]){
+            
+        }else{
+            cout<<"not sorted";
+        }
+    }
+
+
+}
