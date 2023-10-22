90a796765abc9cb9b4de3e0516b9ef906d582cc9
Adithya Gayashan <adithyagayashan02@gmail.com>
2023-10-22 22:28:59 +0530

    merge sort algorithm

---
+++ Python/sorting/mergeSort.py
@@ -0,0 +1,31 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    # Split the input array into two halves
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    # Recursively sort both halves
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+
+    # Merge the sorted halves
+    return merge(left_half, right_half)
+
+def merge(left, right):
+    result = []
+    left_index, right_index = 0, 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
+
+    result.extend(left[left_index:])
+    result.extend(right[right_index:])
+    return result
