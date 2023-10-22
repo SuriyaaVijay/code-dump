d420f71f5c31dbd9d4bff00f9f193ff618124689
Chandan arya <chandan.ug21@nsut.ac.in>
2023-10-22 20:58:22 +0530

    added blog.cpp

--- Block Chain/contracts/blog.cpp
+++ Block Chain/contracts/blog.cpp
@@ -4,40 +4,42 @@
 using namespace eosio;
 using std::string;
 
-class [[eosio::contract]] blog_contract : public eosio::contract {
+class [[eosio::contract]] blog_contract : public eosio::contract
+{
 public:
     using contract::contract;
 
-    [[eosio::action]]
-    void initblog(name authority) {
+    [[eosio::action]] void initblog(name authority)
+    {
         require_auth(authority);
 
         blog_table blogs(get_self(), get_self().value);
         auto iterator = blogs.begin();
-        if (iterator == blogs.end()) {
-            blogs.emplace(authority, [&](auto &new_blog) {
+        if (iterator == blogs.end())
+        {
+            blogs.emplace(authority, [&](auto &new_blog)
+                          {
                 new_blog.blog_id = blogs.available_primary_key();
                 new_blog.current_post_key = 0;
-                new_blog.authority = authority;
-            });
+                new_blog.authority = authority; });
         }
     }
 
-    [[eosio::action]]
-    void signupuser(name authority, string name, string avatar) {
+    [[eosio::action]] void signupuser(name authority, string name, string avatar)
+    {
         require_auth(authority);
 
         user_table users(get_self(), get_self().value);
-        users.emplace(authority, [&](auto &new_user) {
+        users.emplace(authority, [&](auto &new_user)
+                      {
             new_user.user_id = users.available_primary_key();
             new_user.name = name;
             new_user.avatar = avatar;
-            new_user.authority = authority;
-        });
+            new_user.authority = authority; });
     }
 
-    [[eosio::action]]
-    void createpost(name authority, string title, string content, name user) {
+    [[eosio::action]] void createpost(name authority, string title, string content, name user)
+    {
         require_auth(authority);
 
         blog_table blogs(get_self(), get_self().value);
@@ -45,22 +47,22 @@ public:
         eosio::check(blog_itr != blogs.end(), "Blog does not exist. Initialize the blog first.");
 
         post_table posts(get_self(), get_self().value);
-        posts.emplace(authority, [&](auto &new_post) {
+        posts.emplace(authority, [&](auto &new_post)
+                      {
             new_post.post_id = posts.available_primary_key();
             new_post.title = title;
             new_post.content = content;
             new_post.user = user;
             new_post.authority = authority;
-            new_post.pre_post_key = blog_itr->current_post_key;
-        });
+            new_post.pre_post_key = blog_itr->current_post_key; });
 
-        blogs.modify(blog_itr, authority, [&](auto &blog) {
-            blog.current_post_key = posts.available_primary_key();
-        });
+        blogs.modify(blog_itr, authority, [&](auto &blog)
+                     { blog.current_post_key = posts.available_primary_key(); });
     }
 
 private:
-    struct [[eosio::table]] blog {
+    struct [[eosio::table]] blog
+    {
         uint64_t blog_id;
         uint64_t current_post_key;
         name authority;
@@ -69,7 +71,8 @@ private:
     };
     using blog_table = eosio::multi_index<"blogs"_n, blog>;
 
-    struct [[eosio::table]] user {
+    struct [[eosio::table]] user
+    {
         uint64_t user_id;
         string name;
         string avatar;
@@ -79,7 +82,8 @@ private:
     };
     using user_table = eosio::multi_index<"users"_n, user>;
 
-    struct [[eosio::table]] post {
+    struct [[eosio::table]] post
+    {
         uint64_t post_id;
         string title;
         string content;
