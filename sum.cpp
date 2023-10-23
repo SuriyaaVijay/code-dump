4646f3679767d131e8fd79c58f1c5959da97b687
RutuRJP <86903625+RutuRJP@users.noreply.github.com>
2023-10-23 22:18:05 +0530

    C++ Program to find sum of two numbers

---
+++ sum.cpp
@@ -0,0 +1,22 @@
+//Find sum of two numbers
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int T,A,B;
+    
+    cin>>T;
+    
+    while(T>0)
+    {
+        cin>>A>>B;
+        
+        cout<<A+B<<endl;
+        
+        T--;
+    }
+    
+    return 0;
+}
