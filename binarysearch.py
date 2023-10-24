9b6efd20e3797c16dd82f413a0dab44a9d6f33af
Surajith Pranav <91361858+SrSurajithPranav@users.noreply.github.com>
2023-10-24 21:04:23 +0530

    Create binarysearch.py

---
+++ binarysearch.py
@@ -0,0 +1,20 @@
+def binary_search(arr, low, high, x):
+	if high >= low:
+		mid = (high + low) // 2
+		if arr[mid] == x:
+			return mid
+		elif arr[mid] > x:
+			return binary_search(arr, low, mid - 1, x)
+else:
+			return binary_search(arr, mid + 1, high, x)
+	else:
+		# Element is not present in the array
+		return -1
+arr = [ 2, 3, 4, 10, 40 ]
+x = 10
+result = binary_search(arr, 0, len(arr)-1, x)
+
+if result != -1:
+	print("Element is present at index", str(result))
+else:
+	print("Element is not present in array")
