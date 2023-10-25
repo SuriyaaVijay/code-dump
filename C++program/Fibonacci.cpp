b05228bf4e12791d1e7bff0a03fe17b8f3ef744f
princesulekhiya-code <146360988+princesulekhiya-code@users.noreply.github.com>
2023-10-26 00:22:03 +0530

    Update Fibonacci.cpp

--- C++program/Fibonacci.cpp
+++ C++program/Fibonacci.cpp
@@ -1,17 +1,29 @@
-using namespace std;  
+include <iostream>
+using namespace std;
 
-int main()
-{  
- int n1=0,n2=1,n3,i,number;    
- cout<<"Enter the number of elements: ";    
- cin>>number;    
- cout<<n1<<" "<<n2<<" "; 
- for(i=2;i<number;++i)    
- {    
-  n3=n1+n2;    
-  cout<<n3<<" ";    
-  n1=n2;    
-  n2=n3;    
- }    
-   return 0;  
-   }  
+int main() {
+    int n, t1 = 0, t2 = 1, nextTerm = 0;
+
+    cout << "Enter the number of terms: ";
+    cin >> n;
+
+    cout << "Fibonacci Series: ";
+
+    for (int i = 1; i <= n; ++i) {
+        // Prints the first two terms.
+        if(i == 1) {
+            cout << t1 << ", ";
+            continue;
+        }
+        if(i == 2) {
+            cout << t2 << ", ";
+            continue;
+        }
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+        
+        cout << nextTerm << ", ";
+    }
+    return 0;
+}
