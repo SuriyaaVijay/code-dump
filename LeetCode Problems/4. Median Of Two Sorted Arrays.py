3c77db345aa3764d1876437b357ee3b6c3dda017
2412anushka <anushkasy2003@gmail.com>
2023-10-25 15:38:33 +0530

    Changes made in median of two sorted arrays

--- LeetCode Problems/4. Median Of Two Sorted Arrays.py
+++ LeetCode Problems/4. Median Of Two Sorted Arrays.py
@@ -1,11 +1,11 @@
-class Solution(object):
-    def findMedianSortedArrays(self, nums1, nums2):
-        list3 = nums1 + nums2
-        list3.sort()  
-        length = len(list3)
-        if(length % 2 == 0):
-            avg = list3[(length/2)-1]+list3[(length/2)]
-            return avg * 0.5
-        else:
-            return list3[(length-1)/2]
-        return 1
+# class Solution(object):
+def findMedianSortedArrays(nums1, nums2): #Function to find median of two sorted arrays
+    list3 = nums1 + nums2
+    list3.sort()  
+    l = len(list3)
+    if(l % 2 == 0):
+        median=(list3[int(l/2)-1]+list3[int(l/2)])/2
+        return median
+    else:
+        return list3[int(l/2)]
+
