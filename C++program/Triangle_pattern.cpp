37c63fa55475d4af76ce8579ce0c509167ba8b80
princesulekhiya-code <146360988+princesulekhiya-code@users.noreply.github.com>
2023-10-26 00:26:31 +0530

    Update Triangle_pattern.cpp

--- C++program/Triangle_pattern.cpp
+++ C++program/Triangle_pattern.cpp
@@ -1,36 +1,19 @@
 
+// C++ program to print
+// right triangle
 #include <iostream>
 using namespace std;
  
-// Function to demonstrate printing pattern
-void triangle(int n)
-{
-    int i = 0, j = 0, k = 0;
-    while (i < n) {
- 
-        // for spacing
-        while (k <= n - i - 2) {
-            cout << " ";
-            k++;
-        }
-        k = 0;
- 
-        // For Pattern printing
-        while (j <= i) {
-            cout << "* ";
-            j++;
-        }
-        j = 0;
-        i++;
-        cout << endl;
-    }
-}
- 
-
+// Driver code
 int main()
 {
     int n = 5;
-
-    triangle(n);
+ 
+    // ith row has i elements
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++)
+            cout << "* ";
+        cout << endl;
+    }
     return 0;
 }
