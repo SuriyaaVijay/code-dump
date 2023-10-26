88b10ecae0958bdfd20cd5315d093342ae27a406
druhi021204 <readerrox@gmail.com>
2023-10-26 23:56:58 +0530

    Pattern Problem Added

---
+++ C++program/DiamondStarPattern.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void erect_pyramid(int N)
+{
+    // This is the outer loop which will loop for the rows.
+    for (int i = 0; i < N; i++)
+    {
+        // For printing the spaces before stars in each row
+        for (int j =0; j<N-i-1; j++)
+        {
+            cout <<" ";
+        }
+       
+        // For printing the stars in each row
+        for(int j=0;j< 2*i+1;j++){
+            
+            cout<<"*";
+        }
+        
+        // For printing the spaces after the stars in each row
+         for (int j =0; j<N-i-1; j++)
+        {
+            cout <<" ";
+        }
+        
+        // As soon as the stars for each iteration are printed, we move to the
+        // next row and give a line break otherwise all stars
+        // would get printed in 1 line.
+        cout << endl;
+    }
+}
+
+void inverted_pyramid(int N)
+{
+    // This is the outer loop which will loop for the rows.
+    for (int i = 0; i < N; i++)
+    {
+        // For printing the spaces before stars in each row
+        for (int j =0; j<i; j++)
+        {
+            cout <<" ";
+        }
+       
+        // For printing the stars in each row
+        for(int j=0;j< 2*N -(2*i +1);j++){
+            
+            cout<<"*";
+        }
+        
+        // For printing the spaces after the stars in each row
+         for (int j =0; j<i; j++)
+        {
+            cout <<" ";
+        }
+        
+        // As soon as the stars for each iteration are printed, we move to the
+        // next row and give a line break otherwise all stars
+        // would get printed in 1 line.
+        cout << endl;
+    }
+}
+
+int main()
+{   
+    // Here, we have taken the value of N as 5.
+    // We can also take input from the user.
+    int N = 5;
+    erect_pyramid(N);
+    inverted_pyramid(N);
+
+    return 0;
+}
