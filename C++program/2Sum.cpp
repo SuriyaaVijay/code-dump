383b698fedfa13cf2b12230ea5f7310d743e56bc
Divyata Pandey <101250441+divyatapandey@users.noreply.github.com>
2023-10-31 21:47:44 +0530

    Update 2Sum.cpp

--- C++program/2Sum.cpp
+++ C++program/2Sum.cpp
@@ -1,28 +1,14 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-       ios_base::sync_with_stdio(false);
-       cin.tie(NULL);
-       cout.tie(NULL);
-    //    brute force approach
-    //    for(int i=0;i<nums.size();i++){
-    //        for(int j=i+1;j<nums.size();j++){
-    //            if(target==nums[i]+nums[j]){
-    //                return {i,j};
-    //            }
-    //        }
-    //    }
-    //    return {};
-      // Leetcode Question
-    // Below Approach is optimised code  
-       unordered_map<int,int> numIndex;
-       for(int i=0;i<nums.size();i++){
-           int complement=target-nums[i];
-           if(numIndex.find(complement)!=numIndex.end()){
-               return {numIndex[complement],i};
-           }
-           numIndex[nums[i]]=i;
-       }
-       return {};
-    }
-};
+vector<int> twoSum(vector<int>& nums, int target) 
+{
+        //created a map to store numbers with their indices
+        unordered_map<int, int> mp;
+       
+        for(int i = 0; i < nums.size(); i++){
+            if(mp.find(target - nums[i]) == mp.end())
+                mp[nums[i]] = i;
+            else
+                return {mp[target - nums[i]], i};
+        }
+       //returning {-1,-1} if not found
+        return {-1, -1};
+} 
